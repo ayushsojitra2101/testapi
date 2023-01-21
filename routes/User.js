e453f9f5a2fd89aa823fc691c1814f5a44bcc633
ayushsojitra2101 <sojitraayush2101@gmail.com>
2023-01-21 14:13:31 +0530

    new push

--- routes/User.js
+++ routes/User.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var AuthController = require('../Controllers/AuthController');
-// var teamController = require('../Controllers/TeamController')
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-router.post('/login', AuthController.protectGlobal, AuthController.login);
-// router.get('/',AuthController.protectGlobal, AuthController.protect, teamController.get);
-router.post('/changepassword', AuthController.protectGlobal, AuthController.protect, AuthController.ChangePAssword);
-router.post('/forgetpwd', AuthController.protectGlobal, AuthController.mailSending);
-router.post('/signup', AuthController.protectGlobal, AuthController.signUp);
-router.get('/logout', AuthController.protectGlobal, AuthController.protect, AuthController.Logout);
+router.post('/login',  AuthController.login);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const cors = require("cors");
 // const { Socket } = require("socket.io");
 const app = express();
 
-mongoose
-  .connect(
-    "mongodb+srv://dione_apps:Dione&169@cluster0.5yxff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useUnifiedTopology: true, useNewUrlParser: true }
-  )
-  .then(() => console.log("DB connected established"))
-  .catch((err) => console.log("DB connection error: ", err));
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -21,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 const server = require("http").createServer(app);
 
-app.use("/user", require("./routes/User"));
+app.use("/api/v1/users/auth/", require("./routes/User"));
 
 app.use("/", require("./routes/home"));
  
